e1856d34907238c563477aa2a81afd4b6fe0fbbd
ub <3kosko@gmail.com>
2022-05-09 20:20:54 +0200

    update working with numbers

--- sumCountNum.c
+++ sumCountNum.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int CountDigits(int x);
 int SumDigits(int x);
+int Search(int n, int digit);
 
 int main()
 {
 	printf("count = %d\n", CountDigits(243));	
-	printf("sum = %d\n", SumDigits(243));
+	printf("sum = %d\v", SumDigits(243));
+	
+	int n, digit;
+	scanf("%d%d", &n, &digit);
+	printf("%s\n", Search(n, digit) ? "yes" : "no");	
 }
 int CountDigits(int x)
 {
@@ -21,4 +27,11 @@ int SumDigits(int x)
 	while (x /= 10)
 		sum += x % 10;
 	return sum;
+}
+int Search(int n, int digit)
+{
+	n = abs(n);
+	while (n >= 10 && n % 10 != digit)
+		n /= 10;
+	return n >= 10 || n == digit;
 }
